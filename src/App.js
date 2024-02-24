0d7d683422786ef1fb15ea9948bbf4c53770d910
Re-Dei <hridaya.pradhan123@gmail.com>
2024-02-24 18:25:12 +0545

    task 2 complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,29 +19,29 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>    
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>    
                        
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                        <div>
+                            <ExpenseList/>
+                        </div>     
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                        <div>
+                            <ExpenseItem />
+                        </div>    
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                        <div>
+                            <AllocationForm />
+                        </div>     
 
                 </div>
             </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,35 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+  const { budget } = useContext(AppContext);
+  const { expenses } = useContext(AppContext);
+
+  const [newBudget, setNewBudget] = useState(budget);
+  const handleBudgetChange = (event) => {
+    if (event.target.value > 20000) {
+      alert('Value cannot be greater than 20000');
+      return;
+    }
+
+    const totalExpenses = expenses.reduce((total, item) => {
+      return (total += item.cost);
+    }, 0);
+    console.log(totalExpenses)
+
+    if (event.target.value < totalExpenses) {
+      alert('Budget cannot be less than the total expenses');
+      return;
+    }
+
+    setNewBudget(event.target.value);
+  }
+
+  return (
+    <div className='alert alert-secondary'>
+      <span>Budget: £</span>
+      <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+    </div>
+  );
+};
+export default Budget;
